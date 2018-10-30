39d1a5b2d47628679ec56202d632954fe3c2f4ea
jkwon045 <jkwon045@ucr.edu>
2018-10-30 14:53:38 -0700

    Added text field to scene 2

--- src/nearby/lib/main.dart
+++ src/nearby/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,29 +34,81 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: () { Navigator.pushNamed(context, '/second'); },
-        tooltip: 'Go back',
+        tooltip: 'Go Forward',
         child: new Icon(Icons.account_circle),
       ),
     );
   }
 }
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final appTitle = 'test';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: ProfilePageForm(),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () {Navigator.pop(context);},
+          tooltip: 'Go back',
+          child: new Icon(Icons.arrow_back),
+        ),
+      )
+    );
+  }
+}
+
+class ProfilePageForm extends StatefulWidget {
+  @override
+  ProfilePageFormState createState() { return ProfilePageFormState(); }
+}
+
+class ProfilePageFormState extends State<ProfilePageForm>{
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>!
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Screen"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+    // Build a Form widget using the _formKey we created above
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'This is a test',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, we want to show a Snackbar
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
